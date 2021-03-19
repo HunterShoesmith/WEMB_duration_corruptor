501abc0ff75d11f0bce85d0a409495fffd0abd26
Hunter Shoesmith <huntershoesmith@gmail.com>
2021-03-19 06:37:59 +0000

    File input is done. Now takes inp and out and copies.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
+#include <sys/stat.h>
+#include <fstream>
 
-int main() {
-  std::cout << "Hello World!\n";
+
+
+int main(int argc, char* argv[]) {
+
+
+  //checking args
+  if (argc <= 2){
+    std::cout << "please enter a file inp and out in format ./main inp out" << std::endl;
+    return -1;
+  }else{
+    std::cout << "input file path is: " << argv[1] << " output is: " << argv[2] << std::endl;
+  }
+
+
+  std::ifstream  inpFile(argv[1], std::ios::binary);
+
+  std::ofstream  outFile(argv[2], std::ios::binary);
+
+  outFile << inpFile.rdbuf();
+  //copy file to destination with name
+
+  
+
+
+  //get inp and output stream
+
+
 }
